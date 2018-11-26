9a05dd6e0254c0be1f68605d044edc05f314a7ed
George  Pianka <gpianka1@gmail.com>
2018-11-26 13:05:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,10 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/newteam' do
-    raise params.inspect
     erb :newteam
   end
   
   post '/team' do
-    raise params.inspect
     erb :team
   end
   
